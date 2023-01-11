5069763443174f4d33ba044c7e4f4ee8c36c0d0e
Joshua-Coulibaly <joshcoulibaly34@gmail.com>
2023-01-11 11:59:04 -0500

    Added files from school copy

--- src/components/SiteModal.vue
+++ src/components/SiteModal.vue
@@ -1,30 +1,26 @@
 <script setup>
+import axios from 'axios';
 const props = defineProps(["id"]);
 const emits = defineEmits(["toggleModal"]);
-const getMovies = async () => {
-  response.value = (await getData(`https://api.themoviedb.org/3/trending/movie/week`, {
-    params: {
-      api_key: "464558c39f92ed4e861ee35019feed2f",
-      append_to_response: "videos",
-    }
-  })).data['results']
-}
- 
-const getData = async (url, params) => {
-  try {
-    return await axios.get(url, params);
-  } catch (error) {
-    console.log(error)
-  }
-};
+const info = await axios.get(`https://api.themoviedb.org/3/movie/${props.id}`, {
+      params: {
+        api_key: "f944b70daa59b60504fca0c383e63483",
+        append_to_response: "videos"
+      },
+    })
+console.log(info)
 </script>
 
 <template>
+
   <Teleport to="body">
     <div class="modal-outer-container" @click.self="emits('toggleModal')">
       <div class="modal-inner-container">
         <button class="close-button" @click="emits('toggleModal')">X</button>
-        <h1>{{ props.id }}</h1>
+        <img :src="`https://image.tmdb.org/t/p/w500${info.data.poster_path}`" alt="">
+        <p> {{info.data.original_title}}</p>
+        <p> Release Date: {{info.data.release_date}}</p>
+        <iframe :src="`https://www.youtube.com/embed/${info.data.videos.results.filter((video) => video.type === 'Trailer').at(0).key}`"></iframe>
       </div>
     </div>
   </Teleport>
@@ -42,16 +38,17 @@ const getData = async (url, params) => {
   height: 100vh;
   background: #00000099;
   z-index: 3;
+  font-size: 20px;
+  font-family: 'Bangers', cursive;
 }
-
 .modal-outer-container .modal-inner-container {
-  background-color: #1F2123;
+  background-color: rgb(29, 28, 28);
   color: white;
   width: clamp(280px, 100%, 800px);
   height: 400px;
   position: relative;
+  
 }
-
 .modal-outer-container .modal-inner-container .close-button {
   position: absolute;
   right: 0px;
@@ -61,5 +58,16 @@ const getData = async (url, params) => {
   font-weight: bold;
   font-size: 1.25rem;
   color: white;
+}
+img {
+  width: 250px;
+  height: 400px;
+  float: right;
+
+}
+iframe {
+  width: 320px;
+  height: 170px;
+
 }
 </style>
--- src/components/siteHero.vue
+++ src/components/siteHero.vue
@@ -3,7 +3,10 @@
 </script>
 
 <template>
-<h2 class="M">The best movies</h2>
+    <div>
+        <img src="https://www.google.com/imgres?imgurl=https%3A%2F%2Fwallpapercave.com%2Fwp%2Fwp6608538.jpg&imgrefurl=https%3A%2F%2Fwallpapercave.com%2Fmovie-theater-wallpapers&tbnid=52dOL0hA6EYITM&vet=12ahUKEwiilNSGtL38AhVho3IEHcXnBkYQMygJegUIARCCAg..i&docid=HLdf2kiqOskwDM&w=1388&h=880&q=movie%20theatre%20background&ved=2ahUKEwiilNSGtL38AhVho3IEHcXnBkYQMygJegUIARCCAg" alt="MovieBackground">
+<p class="formatp">grwgga <br> grgrgrwa</p>
+    </div>
 </template>
 
 <style scoped>
